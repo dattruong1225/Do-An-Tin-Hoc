972557913019935d27394d5688f0712ded8678fe
kingai <quang17.2000@gmail.com>
2022-07-23 16:38:39 +0700

    fist commit

---
+++ book-management-client/src/App.js
@@ -0,0 +1,17 @@
+import "antd/dist/antd.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Book } from "./pages/Book";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book/:id" element={<Book />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
